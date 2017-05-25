073186e4d9e65266faf337d22e389098ff4610a2
zachzhou777 <zachzhou777@gmail.com>
2017-05-25 17:04:32 -0500

    Edited directly in GitHub

--- Main.java
+++ Main.java
@@ -5,12 +5,39 @@ public class Main {
 	 * classify instances from a testing set.
 	 */
 	public static void main(String[] args) {
-		// TODO
-		// Read the 'semeion.data' file
+		// Read the 'semeion.data' file into 'inputs' and 'desiredOutputs'
+		Scanner stdin = new Scanner(new File("semeion.data"));
+		ArrayList<ArrayList<Double>> inputs = new ArrayList<ArrayList<Double>>();
+		ArrayList<Integer> desiredOutputs = new ArrayList<Integer>();
+		while (stdin.hasNextLine()) {
+			String[] line = stdin.nextLine().split();
+			// Add input for an instance to 'inputs'
+			ArrayList<Double> input = new ArrayList<Double>();
+			for (int i = 0; i < 256; i++) input.add(Double.valueOf(line[i]));
+			inputs.add(input);
+			// Add desired output for an instance to 'desiredOutputs'
+			if (line[256 + 0].equals("1")) desiredOutputs.add(0);
+			else if (line[256 + 1].equals("1")) desiredOutputs.add(1);
+			else if (line[256 + 4].equals("1")) desiredOutputs.add(2);
+			else if (line[256 + 3].equals("1")) desiredOutputs.add(3);
+			else if (line[256 + 4].equals("1")) desiredOutputs.add(4);
+			else if (line[256 + 5].equals("1")) desiredOutputs.add(5);
+			else if (line[256 + 6].equals("1")) desiredOutputs.add(6);
+			else if (line[256 + 7].equals("1")) desiredOutputs.add(7);
+			else if (line[256 + 8].equals("1")) desiredOutputs.add(8);
+			else desiredOutputs.add(9);
+		}
 		
 		// Use a 5:1 ratio of training instances to testing instances
+		int cutoff = 1593 / 5;
+		ArrayList<ArrayList<Double>> testingInputs = inputs.subList(0, cutoff);
+		ArrayList<ArrayList<Double>> trainingInputs = inputs.subList(cutoff, 1593);
+		ArrayList<Integer> testingDesiredOutputs = desiredOutputs.subList(0, cutoff);
+		ArrayList<Integer> trainingDesiredOutputs = desiredOutputs.subList(cutoff, 1593);
 		
 		// Construct and train the network
+		NeuralNet(/*TODO*/);
+		train(trainingInputs, trainingDesiredOutputs, /*TODO*/);
 		
 		// Evaluate the effectiveness of the network
 	}
